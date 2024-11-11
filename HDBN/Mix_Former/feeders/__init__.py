0c9a42054b6fbafa1627a316aa67ac0f8b7ce498
ionicbond1234 <127803454+ionicbond1234@users.noreply.github.com>
2024-11-11 12:41:51 +0800

    Delete HDBN/Mix_Former directory

--- HDBN/Mix_Former/feeders/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import tools
-from . import feeder_ucla
-from . import feeder_ntu
-from . import feeder_uav
--- HDBN/Mix_Former/run.sh
+++
@@ -1,2 +0,0 @@
-CUDA_VISIVLE_DEVICES=1 
-python main.py --config ./config/mixformer_V2_J.yaml
